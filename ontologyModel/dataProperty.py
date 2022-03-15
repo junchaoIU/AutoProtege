49f5d185147eca980e65faffa30a99d558ee299a
Junchao Wu <wujunchaoIU@outlook.com>
2022-03-15 20:29:28 +0800

    add comment, subPropertyOf, domain, range, disjoinDP for DP entity

--- ontologyModel/dataProperty.py
+++ ontologyModel/dataProperty.py
@@ -13,3 +13,143 @@ class DP:
         self.className = className
         self.domainName = domainName
         self.owlLines = owlLines
+
+    def addComment(self, comment):
+        lines = self.owlLines
+        lines.insert(1,
+                     '<rdfs:comment rdf:datatype="http://www.w3.org/2001/XMLSchema#string">' + comment + '</rdfs:comment>')
+        self.owlLines = lines
+        print(lines)
+
+    def updateComment(self, comment):
+        lines = self.owlLines
+        for line in lines:
+            if '<rdfs:comment rdf:datatype="http://www.w3.org/2001/XMLSchema#string">' in line:
+                lines.remove(line)
+        lines.insert(1,
+                     '<rdfs:comment rdf:datatype="http://www.w3.org/2001/XMLSchema#string">' + comment + '</rdfs:comment>')
+        self.owlLines = lines
+        print(lines)
+
+    def deleteComment(self):
+        lines = self.owlLines
+        for line in lines:
+            if '<rdfs:comment rdf:datatype="http://www.w3.org/2001/XMLSchema#string">' in line:
+                lines.remove(line)
+        self.owlLines = lines
+        print(lines)
+
+    def addEquivalentdp(self, equivalentDPName):
+        lines = self.owlLines
+        lines.insert(1, '<owl:equivalentProperty rdf:resource="http://www.owl-ontologies.com/' + self.domainName + '#' + equivalentDPName + '"/>')
+        self.owlLines = lines
+        print(lines)
+
+    def updateEquivalentdp(self, equivalentDPName):
+        lines = self.owlLines
+        for line in lines:
+            if '<owl:equivalentProperty rdf:resource="http://www.owl-ontologies.com/' in line:
+                lines.remove(line)
+        lines.insert(1, '<owl:equivalentProperty rdf:resource="http://www.owl-ontologies.com/' + self.domainName + '#' + equivalentDPName + '"/>')
+        self.owlLines = lines
+        print(lines)
+
+    def deleteEquivalentdp(self):
+        lines = self.owlLines
+        for line in lines:
+            if '<owl:equivalentProperty rdf:resource="http://www.owl-ontologies.com/' in line:
+                lines.remove(line)
+        self.owlLines = lines
+        print(lines)
+
+    def addSubPropertyOf(self, subPropertyOfDPName):
+        lines = self.owlLines
+        lines.insert(1, '<rdfs:subPropertyOf rdf:resource="http://www.owl-ontologies.com/' + self.domainName + '#' + subPropertyOfDPName + '"/>')
+        self.owlLines = lines
+        print(lines)
+
+    def updateSubPropertyOf(self, subPropertyOfDPName):
+        lines = self.owlLines
+        for line in lines:
+            if '<rdfs:subPropertyOf rdf:resource="http://www.owl-ontologies.com/' in line:
+                lines.remove(line)
+        lines.insert(1, '<rdfs:subPropertyOf rdf:resource="http://www.owl-ontologies.com/' + self.domainName + '#' + subPropertyOfDPName + '"/>')
+        self.owlLines = lines
+        print(lines)
+
+    def deleteSubPropertyOf(self):
+        lines = self.owlLines
+        for line in lines:
+            if '<rdfs:subPropertyOf rdf:resource="http://www.owl-ontologies.com/' in line:
+                lines.remove(line)
+        self.owlLines = lines
+        print(lines)
+
+    def addDomain(self, domainName):
+        lines = self.owlLines
+        lines.insert(1, '<rdfs:domain rdf:resource="http://www.owl-ontologies.com/' + self.domainName + '#' + domainName + '"/>')
+        self.owlLines = lines
+        print(lines)
+
+    def updateDomain(self, domainName):
+        lines = self.owlLines
+        for line in lines:
+            if '<rdfs:domain rdf:resource="http://www.owl-ontologies.com/' in line:
+                lines.remove(line)
+        lines.insert(1, '<rdfs:domain rdf:resource="http://www.owl-ontologies.com/' + self.domainName + '#' + domainName + '"/>')
+        self.owlLines = lines
+        print(lines)
+
+    def deleteDomain(self):
+        lines = self.owlLines
+        for line in lines:
+            if '<rdfs:domain rdf:resource="http://www.owl-ontologies.com/' in line:
+                lines.remove(line)
+        self.owlLines = lines
+        print(lines)
+
+    def addRange(self, rangeName):
+        lines = self.owlLines
+        lines.insert(1, '<rdfs:range rdf:resource="http://www.w3.org/2001/XMLSchema#' + rangeName + '"/>')
+        self.owlLines = lines
+        print(lines)
+
+    def updateRange(self, rangeName):
+        lines = self.owlLines
+        for line in lines:
+            if '<rdfs:range rdf:resource="http://www.w3.org/2001/XMLSchema#' in line:
+                lines.remove(line)
+        lines.insert(1, '<rdfs:range rdf:resource="http://www.w3.org/2001/XMLSchema#' + rangeName + '"/>')
+        self.owlLines = lines
+        print(lines)
+
+    def deleteRange(self):
+        lines = self.owlLines
+        for line in lines:
+            if '<rdfs:range rdf:resource="http://www.w3.org/2001/XMLSchema#' in line:
+                lines.remove(line)
+        self.owlLines = lines
+        print(lines)
+
+    def addDisjointOP(self, disjointDPName):
+        lines = self.owlLines
+        lines.insert(1, '<owl:propertyDisjointWith rdf:resource="http://www.owl-ontologies.com/' + self.domainName + '#' + disjointDPName + '"/>')
+        self.owlLines = lines
+        print(lines)
+
+    def updateDisjointOP(self, disjointDPName):
+        lines = self.owlLines
+        for line in lines:
+            if '<<owl:propertyDisjointWith rdf:resource="http://www.owl-ontologies.com/' in line:
+                lines.remove(line)
+        lines.insert(1, '<owl:propertyDisjointWith rdf:resource="http://www.owl-ontologies.com/' + self.domainName + '#' + disjointDPName + '"/>')
+        self.owlLines = lines
+        print(lines)
+
+    def deleteDisjointOP(self):
+        lines = self.owlLines
+        for line in lines:
+            if '<owl:propertyDisjointWith rdf:resource="http://www.owl-ontologies.com/' in line:
+                lines.remove(line)
+        self.owlLines = lines
+        print(lines)
