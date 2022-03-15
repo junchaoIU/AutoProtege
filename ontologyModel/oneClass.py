84fbf5d325a19ec7e59a915cdee63fc2a59bc4e8
Junchao Wu <wujunchaoIU@outlook.com>
2022-03-15 16:55:57 +0800

    add Comment, EquivalentClass and DisjointClass API for class entity

--- ontologyModel/oneClass.py
+++ ontologyModel/oneClass.py
@@ -14,6 +14,52 @@ class OneClass:
         self.domainName = domainName
         self.owlLines = owlLines
 
+    def addComment(self, comment):
+        lines = self.owlLines
+        lines.insert(1, '<rdfs:comment rdf:datatype="http://www.w3.org/2001/XMLSchema#string">' + comment + '</rdfs:comment>')
+        self.owlLines = lines
+        print(lines)
+
+    def updateComment(self, comment):
+        lines = self.owlLines
+        for line in lines:
+            if '<rdfs:comment rdf:datatype="http://www.w3.org/2001/XMLSchema#string">' in line:
+                lines.remove(line)
+        lines.insert(1, '<rdfs:comment rdf:datatype="http://www.w3.org/2001/XMLSchema#string">' + comment + '</rdfs:comment>')
+        self.owlLines = lines
+        print(lines)
+
+    def deleteComment(self):
+        lines = self.owlLines
+        for line in lines:
+            if '<rdfs:comment rdf:datatype="http://www.w3.org/2001/XMLSchema#string">' in line:
+                lines.remove(line)
+        self.owlLines = lines
+        print(lines)
+
+    def addEquivalentClass(self, equivalentClassName):
+        lines = self.owlLines
+        lines.insert(1, '<owl:equivalentClass rdf:resource="http://www.owl-ontologies.com/' + self.domainName + '#' + equivalentClassName + '"/>')
+        self.owlLines = lines
+        print(lines)
+
+    def updateEquivalentClass(self, equivalentClassName):
+        lines = self.owlLines
+        for line in lines:
+            if '<owl:equivalentClass rdf:resource="http://www.owl-ontologies.com/' in line:
+                lines.remove(line)
+        lines.insert(1, '<owl:equivalentClass rdf:resource="http://www.owl-ontologies.com/' + self.domainName + '#' + equivalentClassName + '"/>')
+        self.owlLines = lines
+        print(lines)
+
+    def deleteEquivalentClass(self):
+        lines = self.owlLines
+        for line in lines:
+            if '<owl:equivalentClass rdf:resource="http://www.owl-ontologies.com/' in line:
+                lines.remove(line)
+        self.owlLines = lines
+        print(lines)
+
     def addSuperClass(self, superClassName):
         lines = self.owlLines
         lines.insert(1, '<rdfs:subClassOf rdf:resource="http://www.owl-ontologies.com/' + self.domainName + '#' + superClassName + '"/>')
@@ -33,6 +79,29 @@ class OneClass:
         lines = self.owlLines
         for line in lines:
             if '<rdfs:subClassOf rdf:resource="http://www.owl-ontologies.com/' in line:
+                lines.remove(line)
+        self.owlLines = lines
+        print(lines)
+
+    def addDisjointClass(self, disjointClassName):
+        lines = self.owlLines
+        lines.insert(1, '<owl:disjointWith rdf:resource="http://www.owl-ontologies.com/' + self.domainName + '#' + disjointClassName + '"/>')
+        self.owlLines = lines
+        print(lines)
+
+    def updateDisjointClass(self, disjointClassName):
+        lines = self.owlLines
+        for line in lines:
+            if '<owl:disjointWith rdf:resource="http://www.owl-ontologies.com/' in line:
+                lines.remove(line)
+        lines.insert(1, '<owl:disjointWith rdf:resource="http://www.owl-ontologies.com/' + self.domainName + '#' + disjointClassName + '"/>')
+        self.owlLines = lines
+        print(lines)
+
+    def deleteDisjointClass(self):
+        lines = self.owlLines
+        for line in lines:
+            if '<owl:disjointWith rdf:resource="http://www.owl-ontologies.com/' in line:
                 lines.remove(line)
         self.owlLines = lines
         print(lines)
